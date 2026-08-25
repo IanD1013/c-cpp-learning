0dc423d629b7e5f38c81ea8e600ba60ad2149e69
Ian Dong <ian.dong@maszengrange.co.nz>
2026-08-26 09:10:03 +1200

    sdl - game application and loop (implementation)

--- sdl/main.cpp
+++ sdl/main.cpp
@@ -1,43 +1,86 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 
-int main(int argc, char *argv[])
+struct Application
 {
-    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
-    {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        return 1;
-    }
-
-    // Create our window
-    SDL_Window *window = SDL_CreateWindow("An SDL3 Window", 640, 480, SDL_WINDOW_OPENGL);
-
-    // Our infinite game/application loop
-    bool running = true;
-    while (running)
-    {
-        SDL_Event event;
-        // Processing input
-        while (SDL_PollEvent(&event))
+    public:
+        Application(int argc, char* argv[])
         {
-            if (event.type == SDL_EVENT_QUIT)
+            StartUp();
+        }
+
+        ~Application()
+        {
+            ShutDown();
+        }
+
+        void StartUp()
+        {
+            if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
             {
-                SDL_Log("Program quit %" SDL_PRIu64, event.quit.timestamp);
-                running = false;
+                SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
             }
-            else if (event.type == SDL_EVENT_KEY_DOWN)
+
+            // Create our window
+            mWindow = SDL_CreateWindow("An SDL3 Window", 640, 480, SDL_WINDOW_OPENGL);
+        }
+
+        void ShutDown()
+        {
+            SDL_DestroyWindow(mWindow);
+            SDL_Quit();
+        }
+
+        void Input()
+        {
+            SDL_Event event;
+            // Processing input
+            while (SDL_PollEvent(&event))
             {
-                SDL_Log("Some key was pressed down");
-                if (event.key.key == SDLK_0)
+                if (event.type == SDL_EVENT_QUIT)
                 {
-                    SDL_Log("0 was pressed");
+                    SDL_Log("Program quit %" SDL_PRIu64, event.quit.timestamp);
+                    mRunning = false;
+                }
+                else if (event.type == SDL_EVENT_KEY_DOWN)
+                {
+                    SDL_Log("Some key was pressed down");
+                    if (event.key.key == SDLK_0)
+                    {
+                        SDL_Log("0 was pressed");
+                    }
                 }
             }
         }
-    }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+        void Update()
+        {
+        }
+
+        void Render()
+        {
+        }
+
+        void Loop()
+        {
+            // Our infinite game/application loop
+            while (mRunning)
+            {
+                Input();
+                Update();
+                Render();
+            }
+        }
+
+    private:
+        bool        mRunning { true };
+        SDL_Window* mWindow;
+};
+
+int main(int argc, char* argv[])
+{
+    Application app(argc, argv);
+    app.Loop();
 
     return 0;
 }
